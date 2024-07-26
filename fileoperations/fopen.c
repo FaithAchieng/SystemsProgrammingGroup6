6d846c4bb168f9a83539876ca5331022f8d4ac93
kyalo-45 <117834094+kyalo-45@users.noreply.github.com>
2024-07-26 11:49:22 +0300

    Update fopen.c
    Added comments

--- fileoperations/fopen.c
+++ fileoperations/fopen.c
@@ -1,11 +1,19 @@
-#include<stdio.h>
-#include <string.h>
+#include <stdio.h>  // Include the standard input/output library
+#include <string.h> // Include the string manipulation library
+
 int main()
 {
-FILE *fp;
-fp= fopen("sample.txt" ,
-"wt");
-fprintf(fp, "this is a test file\n");
-fclose(fp);
-return 0;
+    FILE *fp; // Declare a file pointer
+
+    // Open the file "sample.txt" in write text mode ("wt")
+    // If the file doesn't exist, it will be created
+    fp = fopen("sample.txt", "wt");
+
+    // Write the string "this is a test file\n" to the file
+    fprintf(fp, "this is a test file\n");
+
+    // Close the file to ensure all data is written and resources are freed
+    fclose(fp);
+
+    return 0; // Return 0 to indicate successful execution
 }
