bff9759801d431d4fcf5a1fffedfd31605b5d1a4
FaithAchieng <135111683+FaithAchieng@users.noreply.github.com>
2024-07-26 12:21:37 +0300

    Add files via upload

---
+++ fork/fork2.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<sys/types.h>
+#include<unistd.h>
+void forkInstance(){
+if(fork()==0)
+{
+printf("Hello from child");}
+else{
+printf("Hello from parent");
+}}
+int main(){
+forkInstance();
+return 0;
+}
